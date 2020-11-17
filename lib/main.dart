ed0efe783f415e141f69265c2210deab684d3036
Martín Valenzuela <martynvalenz@gmail.com>
2020-11-17 12:49:38 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:estados/pages/pagina1.dart';
+import 'package:estados/pages/pagina2.dart';
+import 'package:flutter/material.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'pagina1',
+      routes:{
+        'pagina1': (_) => Pagina1Page(), 
+        'pagina2': (_) => Pagina2Page()
+      }
+    );
+  }
+}
